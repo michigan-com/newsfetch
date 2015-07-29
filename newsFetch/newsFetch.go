bee5d9c048bb7a44cf84a6a6b3fa0925651b5cb9
Mike Varano <mikevee313@gmail.com>
2015-07-29 16:01:48 -0400

    saving a snapshot (array of articles)

--- newsFetch/newsFetch.go
+++ newsFetch/newsFetch.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -25,6 +26,7 @@ type Photo struct {
 }
 
 type Article struct {
+	Id          int
 	Headline    string
 	Subheadline string
 	Section     string
@@ -36,24 +38,48 @@ type Article struct {
 	Photo       Photo
 }
 
-func getUrl(url string) {
+type Snapshot struct {
+	Articles   []Article
+	Created_at time.Time
+}
+
+func getArticleId(url string) int {
+	// Given an article url, get the ID from it
+	r := regexp.MustCompile("/([0-9]+)/{0,1}$")
+	match := r.FindStringSubmatch(url)
+
+	if len(match) > 1 {
+		i, err := strconv.Atoi(match[1])
+		if err != nil {
+			return -1
+		}
+		return i
+	} else {
+
+		fmt.Println("Failed to get ID from %s", url)
+		return -1
+	}
+}
+
+// Fetch a feed url and parse the articles that get retuned
+// Each successfully parsed article
+func getFeedUrl(url string) []Article {
 	fmt.Println("Fetching %s", url)
+
+	articles := make([]Article, 0)
 
 	resp, err := http.Get(url)
 	if err != nil {
 		log.Print(fmt.Sprintf("Error fetching %s: %e", url, err))
-		return
+		return articles
 	}
+	log.Print(fmt.Sprintf("Successfully fetched %s", url))
 
 	json, err := simplejson.NewFromReader(resp.Body)
 	if err != nil {
 		log.Print(fmt.Sprintf("Error parseing response body for %s: %e", url, err))
-		return
+		return articles
 	}
-
-	session := lib.DBConnect()
-	db := session.DB("mapi")
-	defer session.Close()
 
 	content := json.Get("content")
 	arrContent := content.MustArray()
@@ -62,7 +88,7 @@ func getUrl(url string) {
 	match := replace.FindStringSubmatch(resp.Request.URL.Host)
 	if len(match) < 2 {
 		log.Print(fmt.Println("Could not parse %s for host", resp.Request.URL.Host))
-		return
+		return articles
 	}
 	site := match[1]
 
@@ -70,19 +96,26 @@ func getUrl(url string) {
 		articleJson := content.GetIndex(i)
 		photoAttrs := articleJson.Get("photo_attrs")
 		ssts := articleJson.Get("ssts")
+		articleUrl := fmt.Sprintf("http://%s.com%s", site, articleJson.Get("url").MustString())
+		articleId := getArticleId(articleUrl)
+
+		// Check to make sure we could parse the ID
+		if articleId < 0 {
+			continue
+		}
 
 		//fmt.Println("Saving article %s", article.headline)
-
-		err = db.C("articles").Insert(&Article{
-			articleJson.Get("headline").MustString(),
-			articleJson.Get("attrs").Get("brief").MustString(),
-			ssts.Get("section").MustString(),
-			ssts.Get("subsection").MustString(),
-			site,
-			articleJson.Get("summary").MustString(),
-			time.Now(),
-			fmt.Sprintf("http://%s.com%s", site, articleJson.Get("url").MustString()),
-			Photo{
+		article := Article{
+			Id:          articleId,
+			Headline:    articleJson.Get("headline").MustString(),
+			Subheadline: articleJson.Get("attrs").Get("brief").MustString(),
+			Section:     ssts.Get("section").MustString(),
+			Subsection:  ssts.Get("subsection").MustString(),
+			Source:      site,
+			Summary:     articleJson.Get("summary").MustString(),
+			Created_at:  time.Now(),
+			Url:         articleUrl,
+			Photo: Photo{
 				photoAttrs.Get("caption").MustString(),
 				photoAttrs.Get("credit").MustString(),
 				PhotoInfo{
@@ -96,14 +129,12 @@ func getUrl(url string) {
 					photoAttrs.Get("simageheight").MustInt(),
 				},
 			},
-		})
-
-		if err != nil {
-			panic(err)
 		}
+
+		articles = append(articles, article)
 	}
 
-	log.Print(fmt.Sprintf("Successfully fetched %s", url))
+	return articles
 }
 
 func formatUrls() []string {
@@ -131,9 +162,39 @@ func formatUrls() []string {
 func FetchArticles() {
 	log.Print("Fetching articles")
 
+	// Fetch articles from urls
 	urls := formatUrls()
+	c := make(chan []Article)
+	articles := make([]Article, 0)
 	for i := 0; i < len(urls); i++ {
 
-		go getUrl(urls[i])
+		go func(url string, c chan<- []Article) {
+			c <- getFeedUrl(url)
+
+		}(urls[i], c)
 	}
+
+	// Wait until all return and parse the results
+	for i := 0; i < len(urls); i++ {
+		returnedArticles := <-c
+		for j := 0; j < len(returnedArticles); j++ {
+			articles = append(articles, returnedArticles[j])
+		}
+	}
+
+	// DB stuff
+	session := lib.DBConnect()
+	defer session.Close()
+
+	snapshotCollection := session.DB("mapi").C("Snapshot")
+	err := snapshotCollection.Insert(&Snapshot{
+		Articles:   articles,
+		Created_at: time.Now(),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	log.Print("Saved a batch of articles")
 }
