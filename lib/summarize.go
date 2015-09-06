bb95e48ecf82295661711f7d3851eeb57e17e730
Eric Bower <neurosnap@gmail.com>
2015-09-06 14:17:57 -0400

    override annotate tokens for extra goodness

--- lib/summarize.go
+++ lib/summarize.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"fmt"
 	"github.com/neurosnap/sentences/data"
 	"github.com/neurosnap/sentences/punkt"
 	"github.com/urandom/text-summary/summarize"
@@ -10,6 +11,17 @@ type PunktTextSplitter struct {
 	summarize.DefaultTextSplitter
 }
 
+type SentenceTokenizer struct {
+	punkt.SentenceTokenizer
+}
+
+func (s *SentenceTokenizer) AnnotateTokens(tokens []*punkt.Token) []*punkt.Token {
+	tokens = s.AnnotateFirstPass(tokens)
+	tokens = s.AnnotateSecondPass(tokens)
+	fmt.Println("HI I ACTUALLY GOT HIT\n------------")
+	return tokens
+}
+
 func (p PunktTextSplitter) Sentences(text string) []string {
 	b, err := data.Asset("data/english.json")
 	if err != nil {
@@ -17,7 +29,17 @@ func (p PunktTextSplitter) Sentences(text string) []string {
 	}
 	training, err := punkt.LoadTraining(b)
 
-	tokenizer := punkt.NewSentenceTokenizer(training)
+	//tokenizer := punkt.NewSentenceTokenizer(training)
+
+	tokenizer := SentenceTokenizer{
+		punkt.SentenceTokenizer{
+			Base:        punkt.NewBase(),
+			Punctuation: punkt.Punctuation,
+		},
+	}
+
+	tokenizer.Storage = training
+	tokenizer.STokenizer = &tokenizer
 
 	return tokenizer.Tokenize(text)
 }
