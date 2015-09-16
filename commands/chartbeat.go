fc9795b5bcf31714ddde3e8a0980a9d31b3f554f
migreva <mikevee313@gmail.com>
2015-09-16 16:15:47 -0400

    supervisor conf, added environment variable support

--- commands/chartbeat.go
+++ commands/chartbeat.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/michigan-com/newsfetch/lib"
@@ -16,40 +17,50 @@ var cmdChartbeat = &cobra.Command{
 var cmdTopPages = &cobra.Command{
 	Use:   "toppages",
 	Short: "Fetch toppages snapshot for Chartbeat",
-	Run: func(cmd *cobra.Command, args []string) {
-		if timeit {
-			startTime = time.Now()
-		}
+	Run:   ChartbeatToppages,
+}
 
-		if verbose {
-			Verbose("")
-		}
+func ChartbeatToppages(cmd *cobra.Command, args []string) {
+	// Set up environment
+	if mongoUri == "" {
+		mongoUri = os.Getenv("MONGO_URI")
+	}
+	if apiKey == "" {
+		apiKey = os.Getenv("CHARTBEAT_API_KEY")
+	}
 
-		logger.Info("Fetching toppages")
-		urls, err := lib.FormatChartbeatUrls("live/toppages/v3", lib.Sites, apiKey)
+	if timeit {
+		startTime = time.Now()
+	}
+
+	if verbose {
+		Verbose("")
+	}
+
+	logger.Info("Fetching toppages")
+	urls, err := lib.FormatChartbeatUrls("live/toppages/v3", lib.Sites, apiKey)
+	if err != nil {
+		panic(err)
+	}
+
+	snapshot := lib.FetchTopPages(urls)
+
+	if mongoUri != "" {
+		logger.Info("Saving toppages snapshot")
+		err := lib.SaveTopPagesSnapshot(mongoUri, snapshot)
+
 		if err != nil {
 			panic(err)
 		}
 
-		snapshot := lib.FetchTopPages(urls)
-
-		if mongoUri != "" {
-			logger.Info("Saving toppages snapshot")
-			err := lib.SaveTopPagesSnapshot(mongoUri, snapshot)
-
-			if err != nil {
-				panic(err)
-			}
-
-			// Update mapi to let it know that a new snapshot has been saved
-			_, err = http.Get("https://api.michigan.com/popular/")
-			if err != nil {
-				logger.Error("%v", err)
-			} else {
-				logger.Info("Updated snapshot at Mapi at %v", time.Now())
-			}
+		// Update mapi to let it know that a new snapshot has been saved
+		_, err = http.Get("https://api.michigan.com/popular/")
+		if err != nil {
+			logger.Error("%v", err)
 		} else {
-			logger.Warning("Variable 'mongoUri' not specified, no data will be saved")
+			logger.Info("Updated snapshot at Mapi at %v", time.Now())
 		}
-	},
+	} else {
+		logger.Warning("Variable 'mongoUri' not specified, no data will be saved")
+	}
 }
