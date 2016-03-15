594af7daed1e071e9de0396972c3172cc6296dfb
Bendik Lynghaug <bendik.lynghaug@gmail.com>
2016-03-15 11:22:58 -0400

    add unit tests for historical command

--- commands/historical.go
+++ commands/historical.go
@@ -20,16 +20,14 @@ type ByVisits []h.Referrer
 func (a ByVisits) Len() int { return len(a) }
 func (a ByVisits) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a ByVisits) Less(i, j int) bool { return a[i].TotalViewers > a[j].TotalViewers }
-
 var MIN_VISITS = 5.0
 
 var cmdHistorical = &cobra.Command{
 	Use:   "historical",
 	Short: "Compile a history data to keep track of", Run: func(cmd *cobra.Command, args []string) {
 		startTime = time.Now()
-    referrerSnapshot := m.ReferrersSnapshot{}
+    referrerSnapshot := &m.ReferrersSnapshot{}
     var newRecord = h.HistoricalEntry{}
-    var referrerMap = make(map[string]h.Referrer)
   	var session *mgo.Session
   	if globalConfig.MongoUrl != "" {
   		session = lib.DBConnect(globalConfig.MongoUrl)
@@ -43,48 +41,8 @@ var cmdHistorical = &cobra.Command{
       return
     }
 
-    for _, sourceRef := range referrerSnapshot.Referrers {
-      source := sourceRef.Source
-			histDebugger.Printf("snapshot is %s", referrerSnapshot)
-
-      for site, value := range sourceRef.Referrers {
-        numValue := value.(float64)
-
-				histDebugger.Printf("%s->%s: %d", source, site, numValue)
-
-        if _, ok := referrerMap[site]; !ok {
-          pubViews := make ([]bson.M, 0,  len(referrerSnapshot.Referrers))
-          referrerMap[site] = h.Referrer{
-            Site: site,
-            TotalViewers: numValue,
-            PublicationsCount: append(pubViews, bson.M{
-              "source": source,
-              "viewers": numValue,
-            }),
-          }
-        } else {
-          _ref := referrerMap[site]
-          _ref.TotalViewers += numValue
-          _ref.PublicationsCount = append(_ref.PublicationsCount, bson.M{
-            "source": source,
-            "viewers": numValue,
-          })
-
-					referrerMap[site] = _ref
-        }
-      }
-    }
-
-    referrers := make([]h.Referrer, 0, len(referrerMap))
-    for _, ref := range referrerMap {
-      if ref.TotalViewers >= MIN_VISITS {
-        referrers = append(referrers, ref)
-      }
-    }
-    sort.Sort(ByVisits(referrers))
-
     newRecord.Timestamp = time.Now()
-    newRecord.Referrers = referrers
+    newRecord.Referrers = processReferrerSnapshot(referrerSnapshot)
 
     saveCollection := session.DB("").C("History")
 
@@ -95,4 +53,48 @@ var cmdHistorical = &cobra.Command{
     }
 		getElapsedTime(&startTime)
 	},
+}
+
+func processReferrerSnapshot(referrerSnapshot *m.ReferrersSnapshot) []h.Referrer  {
+	var referrerMap = make(map[string]h.Referrer)
+	for _, sourceRef := range referrerSnapshot.Referrers {
+		source := sourceRef.Source
+		histDebugger.Printf("snapshot is %s", referrerSnapshot)
+
+		for site, value := range sourceRef.Referrers {
+			numValue := value.(float64)
+
+			histDebugger.Printf("%s->%s: %d", source, site, numValue)
+
+			if _, ok := referrerMap[site]; !ok {
+				pubViews := make ([]bson.M, 0,  len(referrerSnapshot.Referrers))
+				referrerMap[site] = h.Referrer{
+					Site: site,
+					TotalViewers: numValue,
+					PublicationsCount: append(pubViews, bson.M{
+						"source": source,
+						"viewers": numValue,
+					}),
+				}
+			} else {
+				_ref := referrerMap[site]
+				_ref.TotalViewers += numValue
+				_ref.PublicationsCount = append(_ref.PublicationsCount, bson.M{
+					"source": source,
+					"viewers": numValue,
+				})
+
+				referrerMap[site] = _ref
+			}
+		}
+	}
+
+	referrers := make([]h.Referrer, 0, len(referrerMap))
+	for _, ref := range referrerMap {
+		if ref.TotalViewers >= MIN_VISITS {
+			referrers = append(referrers, ref)
+		}
+	}
+	sort.Sort(ByVisits(referrers))
+	return referrers
 }
