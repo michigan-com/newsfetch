f774aca4bc242fded4641bc0b5ca03ddbe3b80c4
Bendik Lynghaug <bendik.lynghaug@gmail.com>
2016-03-24 17:34:39 -0400

    modify saving referrers snapshot to have an `expire_at` attribute and also save it to ReferrersHistory

--- model/chartbeat/referrers.go
+++ model/chartbeat/referrers.go
@@ -11,17 +11,28 @@ type ReferrersSnapshot struct {
 	Id         bson.ObjectId `bson:"_id,omitempty"`
 	Created_at time.Time     `bson:"created_at"`
 	Referrers  []*Referrers  `bson:"referrers"`
+	Expire_at  time.Time     `bson:"expire_at"`
 }
 
 func (r ReferrersSnapshot) Save(session *mgo.Session) {
   collection := session.DB("").C("Referrers")
+  historyCollection := session.DB("").C("ReferrerHistory")
+	r.Expire_at = r.Created_at.Add(time.Duration(30)* time.Second)
   err := collection.Insert(r)
-
   if err != nil {
     debugger.Printf("Failed to insert Referrers snapshot: %v", err)
     return
   }
 
+	latest := &ReferrersSnapshot{};
+
+	err = historyCollection.Find(bson.M{}).Sort("created_at").One(latest)
+	if err == Error("not found") || latest.Created_at.After(time.Now().Add(time.Duration(5)* time.Minute)) {
+    debugger.Printf("Saved a Snapshot to ReferrerHistory Collection")
+		r.Expire_at = r.Expire_at.Add(time.Duration(24*7)* time.Hour)
+		historyCollection.Insert(r)
+	}
+	// remove once indexes are inforced
   removeOldSnapshots(collection)
 }
 
